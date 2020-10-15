d43ca4733cc7d5002aa19c4b78ec1a50fcb622ba
EliaPiccoli <eliakoby@live.it>
2020-10-15 13:13:29 +0200

    full dataset and first check test

--- test.py
+++ test.py
@@ -9,17 +9,16 @@
 """
 dt = []
 first = True
-count = 0
 for line in open('customer_supermarket.csv'):
-    if count > 10:
-        print(*dt, sep="\n")
-        exit(1)
     if first:
         titles = line.strip().split("\t")
         first = False
     else:
-        count += 1
         elem = {}
         for i in range(len(titles)):
             elem[titles[i]] = line.strip().split("\t")[i+1]
         dt.append(elem)
+
+print(len(dt))
+
+print(sum(1 for row in dt if int(row['Qta']) < 0))
