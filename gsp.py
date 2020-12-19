59cdb8efcac5423cca19ae8e41acee51dcf53c11
EliaPiccoli <eliakoby@live.it>
2020-12-19 12:53:18 +0100

    at least we tried

--- gsp.py
+++ gsp.py
@@ -1,12 +1,15 @@
 # coding: utf-8
-
-
 # # Preliminaries
 import copy
 import itertools
 from collections import defaultdict
 from operator import itemgetter
-import tqdm
+
+from joblib import Parallel, delayed
+import multiprocessing
+import os
+import time
+from tqdm import trange
 
 # #### Our dataset format
 # An event is a list of strings.
@@ -26,20 +29,26 @@ import tqdm
 # # Foundations
 # ### Subsequences
 
+# ### Support of a sequence
+"""
+Computes the support of a sequence in a dataset
+"""
+def countSupport(dataset, candidateSequence):
+    # num_cores = multiprocessing.cpu_count()
+    # results = Parallel(n_jobs=num_cores)(delayed(isSubsequence)(seq, candidateSequence) for seq in dataset)    
+    # return sum(1 for res in results if res)
+    return sum(1 for seq in dataset if isSubsequence(seq, candidateSequence))
+
+
+
 #This is a simple recursive method that checks if subsequence is a subSequence of mainSequence
-
-
-
 def isSubsequence(mainSequence, subSequence):
     subSequenceClone = list(subSequence)  # clone the sequence, because we will alter it
     return isSubsequenceRecursive(mainSequence, subSequenceClone)  # start recursion
 
-
 """
 Function for the recursive call of isSubsequence, not intended for external calls
 """
-
-
 def isSubsequenceRecursive(mainSequence, subSequenceClone, start=0):
     # Check if empty: End of recursion, all itemsets have been found
     if (not subSequenceClone):
@@ -47,43 +56,32 @@ def isSubsequenceRecursive(mainSequence, subSequenceClone, start=0):
     # retrieves element of the subsequence and removes is from subsequence
     firstElem = set(subSequenceClone.pop(0))
     # Search for the first itemset...
-    for i in tqdm.tqdm(range(start, len(mainSequence)), total=len(range(start, len(mainSequence))), desc="Iterating over customers", leave = False):
+    for i in range(start, len(mainSequence)):
         if (set(mainSequence[i]).issuperset(firstElem)):
             # and recurse
             return isSubsequenceRecursive(mainSequence, subSequenceClone, i + 1)
     return False
 
-
-# ### Size of sequences
-"""
-Computes the size of the sequence (sum of the size of the contained elements)
-"""
-
-
-def sequenceSize(sequence):
-    return sum(len(i) for i in sequence)
-
-
-# ### Support of a sequence
-"""
-Computes the support of a sequence in a dataset
-"""
-
-
-def countSupport(dataset, candidateSequence):
-    #print(f"\nCOUNTSUPPORT\n dataset:{dataset}, cand:{candidateSequence}")
-    return sum(1 for seq in dataset if isSubsequence(seq, candidateSequence))
-
+def isSubsequenceIterative(mainSequence, subSequenceClone):
+    start = 0
+    while subSequenceClone:
+        found = False
+        nextElem = subSequenceClone.pop(0)
+        for i in range(start, len(mainSequence)):
+            if (set(mainSequence[i]).issuperset(nextElem)):
+                found = True
+                start = i+1
+                break
+        if not found:
+            return False
+    return True
 
 # # AprioriAll
 # ### 1 . Candidate Generation
-
 # #### For a single pair:
 """
 Generates one candidate of size k from two candidates of size (k-1) as used in the AprioriAll algorithm
 """
-
-
 def generateCandidatesForPair(cand1, cand2):
     cand1Clone = copy.deepcopy(cand1)
     cand2Clone = copy.deepcopy(cand2)
@@ -109,13 +107,17 @@ def generateCandidatesForPair(cand1, cand2):
             newCandidate[-1].extend(cand2[-1][-1])
         return newCandidate
 
+# ### Size of sequences
+"""
+Computes the size of the sequence (sum of the size of the contained elements)
+"""
+def sequenceSize(sequence):
+    return sum(len(i) for i in sequence)
 
 # #### For a set of candidates (of the last level):
 """
 Generates the set of candidates of size k from the set of frequent sequences with size (k-1)
 """
-
-
 def generateCandidates(lastLevelCandidates):
     k = sequenceSize(lastLevelCandidates[0]) + 1
     if (k == 2):
@@ -139,8 +141,6 @@ def generateCandidates(lastLevelCandidates):
 Computes all direct subsequence for a given sequence.
 A direct subsequence is any sequence that originates from deleting exactly one item from any element in the original sequence.
 """
-
-
 def generateDirectSubsequences(sequence):
     result = []
     for i, itemset in enumerate(sequence):
@@ -159,8 +159,6 @@ def generateDirectSubsequences(sequence):
 """
 Prunes the set of candidates generated for size k given all frequent sequence of level (k-1), as done in AprioriAll
 """
-
-
 def pruneCandidates(candidatesLastLevel, candidatesGenerated):
     return [cand for cand in candidatesGenerated if
             all(x in candidatesLastLevel for x in generateDirectSubsequences(cand))]
@@ -177,55 +175,59 @@ Args:
 Returns:
     A list of tuples (s, c), where s is a frequent sequence, and c is the count for that sequence
 """
-
 def apriori(dataset, minSupport, verbose=False):
-    print("Verbose:",verbose)
+    start = time.time()
     global numberOfCountingOperations
     numberOfCountingOperations = 0
     Overall = []
-    itemsInDataset = sorted(set([item for sublist1 in dataset for item in sublist1]))
-    print("itemsInDataset: ", itemsInDataset)
-    #print("Alex")
-    singleItemSequences = [[item] for item in itemsInDataset] # is usefull [item] (?)
-    print("singleItemSequences: ", singleItemSequences)
-    #exit()
-    #computes 2 times recursive shit ggwp
-    #singleItemCounts = [(i, countSupport(dataset, i)) for i in singleItemSequences if countSupport(dataset, i) >= minSupport]
+    itemsInDataset = sorted(set([item for sublist1 in dataset for sublist2 in sublist1 for item in sublist2])) # is necessary? 
+    singleItemSequences = [[[item]] for item in itemsInDataset] # creates starting singleton
     singleItemCounts = []
-    for i in singleItemSequences:
-        x = countSupport(dataset, i)
+    for item in singleItemSequences:
+        x = countSupport(dataset, item)
         if x >= minSupport:
-            singleItemCounts.append((i, x))
-    #print("EP")
+            singleItemCounts.append((item, x))
     Overall.append(singleItemCounts)
     if verbose:
         print("Result, lvl 1: " + str(Overall[0]))
-
-    print("\nsingleItemCounts: ", singleItemCounts)
-    return
-    
     k = 1
-    while (True):
+    while True:
         if not Overall[k - 1]:
             break
         # 1. Candidate generation
         candidatesLastLevel = [x[0] for x in Overall[k - 1]]
         candidatesGenerated = generateCandidates(candidatesLastLevel)
+        if verbose:
+            print("Candidates generated, lvl ", k + 1)
         # 2. Candidate pruning (using a "containsall" subsequences)
-        candidatesPruned = [cand for cand in candidatesGenerated if
-                            all(x in candidatesLastLevel for x in generateDirectSubsequences(cand))]
+        candidatesPruned = [cand for cand in candidatesGenerated if all(x in candidatesLastLevel for x in generateDirectSubsequences(cand))]
+        if verbose:
+            print("Candidates pruned, lvl ", k + 1)
         # 3. Candidate checking
-        candidatesCounts = [(i, countSupport(dataset, i)) for i in candidatesPruned]
+        candidatesCounts = []
+        tot = len(candidatesPruned)
+        for i in trange(tot):
+            candidatesCounts.append((candidatesPruned[i], countSupport(dataset, candidatesPruned[i])))
         resultLvl = [(i, count) for (i, count) in candidatesCounts if (count >= minSupport)]
         if verbose:
-            print("Candidates generated, lvl " + str(k + 1) + ": " + str(candidatesGenerated))
-            print("Candidates pruned, lvl " + str(k + 1) + ": " + str(candidatesPruned))
-            print("Result, lvl " + str(k + 1) + ": " + str(resultLvl))
+            print("Result, lvl ", k + 1)
         Overall.append(resultLvl)
         k = k + 1
     # "flatten" Overall
     Overall = Overall[:-1]
     Overall = [item for sublist in Overall for item in sublist]
+    end = time.time()
+    print("Total Time", end-start)
     return Overall
 
+if __name__ == "__main__":
+    with open('5.sequences_of_poits.text') as f:
+        content = f.readlines()
+    content = [ [ [event] for event in x.strip().split()[:-1] ] for x in content]
 
+    print("Number of input sequences: ", len(content))
+    print("Total number of events: ", sum([len(seq) for seq in content])) 
+    res3 = apriori(content, 30, verbose=True)
+    print(len(res3))
+    print(res3)
+                                             
